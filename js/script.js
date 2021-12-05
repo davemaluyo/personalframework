df27de7317675abb5ec9e45d180f20006ae402f2
Dave Maluyo <dave.maluyo@gmail.com>
2021-12-04 20:27:45 -1000

    animation js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// REVEAL ON SCROLL JS
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute('data-sectionstate', 'active');
+        } else {
+            entry.target.setAttribute('data-sectionstate', 'inactive');
+        }
+    });
+});
+document.querySelectorAll('.animate-on-scroll').forEach((section) => {
+    myobserver.observe(section);
+});
